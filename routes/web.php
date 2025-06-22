377eb0a4dddb2d135c71f2f9212ceee6a623113d
BernatDev6 <bernifogi@gmail.com>
2025-06-22 12:02:31 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Test route
-Route::get('/test', function () {
-    return response()->json(['message' => 'Laravel está funcionando']);
+Route::get('/', function () {
+    return view('welcome');
 });
